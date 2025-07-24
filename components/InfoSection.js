23b66b8ad7007a12fe6ba46e27b7da93157a1331
Jorge Malagon <jorgeh.malagonv@gmail.com>
2025-07-24 17:22:09 +0900

    Added images to the landing page.

--- components/InfoSection.js
+++ components/InfoSection.js
@@ -1,14 +1,26 @@
 import React from "react";
 import useScrollReveal from "./useScrollReveal";
 
-export default function InfoSection({ icon, title, text }) {
+export default function InfoSection({ icon, title, text, image }) {
   const revealRef = useScrollReveal();
   return (
-    <div ref={revealRef} className="info-section fade-up-scroll">
-      <h3 className="flex items-center mb-2">
-        <i className={`fas ${icon} mr-2`} /> {title}
-      </h3>
-      <p className="info-text">{text}</p>
+    <div
+      ref={revealRef}
+      className="info-section fade-up-scroll flex items-start"
+    >
+      <div>
+        <h3 className="flex items-center mb-2">
+          <i className={`fas ${icon} mr-2`} /> {title}
+        </h3>
+        <p className="info-text">{text}</p>
+      </div>
+      {image && (
+        <img
+          src={`/img/${image}`}
+          alt={`${title} illustration for JDM enthusiasts`}
+          className="ml-4 w-24 h-24 object-cover rounded"
+        />
+      )}
     </div>
   );
 }
